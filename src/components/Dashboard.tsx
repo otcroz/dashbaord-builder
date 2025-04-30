b57a33b815a44a80c86a0e1fd3618534c60f1f9a
otcroz <dbtndus0527@gmail.com>
2025-04-30 22:18:56 +0900

    [fix/style] 테마 모드에 따른 버튼 텍스트 반영 - 라이트 모드, 다크 모드 버튼 다르게 보이도록 수정

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -33,7 +33,7 @@ const Dashboard = () => {
                 <Button
                     onClick={() => (theme == 'light' ? changeTheme('dark') : changeTheme('light'))}
                 >
-                    모드
+                    {theme == 'light' ? '다크 모드로 변경' : '라이트 모드로 변경'}
                 </Button>
                 <Button onClick={() => saveJSONFile()}>현재 상태 저장하기</Button>
                 <Button onClick={() => loadJSONFile()}>위젯 불러오기</Button>
